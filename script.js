9d1454a7003d5ef57ed676c8093fa7d204bf499c
Cesar Pereira <105113840+profcesarPC@users.noreply.github.com>
2025-06-24 19:31:46 -0300

    ff

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+function corAleatoria() {
+    // Função para gerar uma cor aleatória
+    const letras = "0123456789ABCDEF";
+    let cor = "#";
+    for (let i = 0; i < 6; i++) {
+        cor += letras[Math.floor(Math.random() * 16)];
+    }
+    return cor;
+}
+
 function inserirNome() {
-	let nomeUsuario = prompt ("Qual o seu nome?");
-	let elemento = document.querySelector("#nome-usuário");
-	elemento.textContent = nomeUsuario;
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector("#nome-usuário");
+    elemento.textContent = nomeUsuario;
+    elemento.style.color = corAleatoria(); // Define a cor inicial ao exibir o nome
+    elemento.onclick = function() {
+        elemento.style.color = corAleatoria(); // Muda a cor toda vez que o nome for clicado
+    };
 }
 
 inserirNome();
