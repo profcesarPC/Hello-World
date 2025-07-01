3148f7327f02d713a0d22455feb34944dfda6991
Cesar Pereira <105113840+profcesarPC@users.noreply.github.com>
2025-07-01 18:58:44 -0300

    Criação de listas

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ function inserirNome() {
 }
 
 inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C++"];
+
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
+
